805038395e547366678831568fde866817546897
Elias <elias.juriatti@outlook.com>
2026-08-29 12:55:06 -0300

    test(ui): cover placeholder artwork alignment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,17 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:musify/main.dart';
+import 'package:musify/widgets/no_artwork_cube.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const Musify());
+  testWidgets('centers the placeholder music note optically', (
+    WidgetTester tester,
+  ) async {
+    await tester.pumpWidget(
+      const MaterialApp(home: NullArtworkWidget(size: 100, iconSize: 25)),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final transform = tester.widget<Transform>(find.byType(Transform));
+    expect(transform.transform.getTranslation().x, -1);
+    expect(transform.transform.getTranslation().y, -1);
   });
 }
